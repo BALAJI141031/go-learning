8f0e73b7de8d908df79f26170cec909ad0fb7c04
balajiNarayana49 <balaji.narayana@kenko-health.in>
2023-12-02 08:16:06 +0530

    lexer and default go path comments

--- 01print/printStatement.go
+++ 01print/printStatement.go
@@ -5,3 +5,14 @@ import "fmt"
 func main() {
 	fmt.Println("Hello World")
 }
+
+// FINDING env path in go
+/*
+to know the default path that setup by the go for env , type -> go env GOPATH
+
+LEXER IN GOLANG:
+	it is responsible for converting stream of characters into tokens and given to further pipeline stages as a input
+
+Variables,Types,Constants:
+
+*/
